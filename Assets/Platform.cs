db34af68d48d77cbc1e07121f199c00cc1ea5efc
Robert <rob@Roberts-MacBook-Pro.local>
2018-04-13 00:56:01 +0200

    Added validation if player comes from above

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -6,11 +6,14 @@ public class Platform : MonoBehaviour {
 	public float jumpForce = 10f;
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-		if(rb != null){
-			Vector2 velocity = rb.velocity;
-			velocity.y = jumpForce;
-			rb.velocity = velocity;
+		if (collision.relativeVelocity.y <= 0f){
+			Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+			if (rb != null)
+			{
+				Vector2 velocity = rb.velocity;
+				velocity.y = jumpForce;
+				rb.velocity = velocity;
+			}
 		}
 	}
 }
